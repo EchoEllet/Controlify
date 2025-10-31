02768f2c438513cd0748657884cfc2a3ca15c585
Ellet <echo.ellet@gmail.com>
2025-10-31 15:14:36 +0300

    fix: all game launch crashes when running on production environment

--- src/main/java/dev/isxander/controlify/mixins/feature/rumble/blockbreak/MultiPlayerGameModeMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/rumble/blockbreak/MultiPlayerGameModeMixin.java
@@ -3,7 +3,6 @@ package dev.isxander.controlify.mixins.feature.rumble.blockbreak;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.isxander.controlify.api.ControlifyApi;
-import dev.isxander.controlify.controller.ControllerEntity;
 import dev.isxander.controlify.rumble.ContinuousRumbleEffect;
 import dev.isxander.controlify.rumble.RumbleSource;
 import dev.isxander.controlify.rumble.RumbleState;
@@ -22,75 +21,66 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
-    @Unique private ContinuousRumbleEffect blockBreakRumble = null;
+    @Unique
+    private ContinuousRumbleEffect controlify$blockBreakRumble = null;
 
     @Inject(
-            //? if fabric {
-            /*method = "method_41930",
-            *///?} else {
             method = "lambda$startDestroyBlock$1",
-            //?}
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;destroyBlockProgress(ILnet/minecraft/core/BlockPos;I)V")
     )
     private void onStartBreakingBlock(CallbackInfoReturnable<Packet<?>> cir, @Local(argsOnly = true) BlockState state) {
-        startRumble(state);
+        controlify$startRumble(state);
     }
 
     @Inject(
-            //? if fabric {
-            /*method = "method_41930",
-            *///?} else {
             method = "lambda$startDestroyBlock$1",
-            //?}
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;destroyBlock(Lnet/minecraft/core/BlockPos;)Z")
     )
     private void onInstabreakBlockSurvival(CallbackInfoReturnable<Packet<?>> cir, @Local(argsOnly = true) BlockState state) {
-        startRumble(state);
+        controlify$startRumble(state);
         // won't stop until 1 tick
-        stopRumble();
+        controlify$stopRumble();
     }
 
     @Inject(method = "stopDestroyBlock", at = @At("RETURN"))
     private void onStopBreakingBlock(CallbackInfo ci) {
-        stopRumble();
+        controlify$stopRumble();
     }
 
     @Inject(method = "continueDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;startPrediction(Lnet/minecraft/client/multiplayer/ClientLevel;Lnet/minecraft/client/multiplayer/prediction/PredictiveAction;)V", ordinal = 1))
     private void onFinishBreakingBlock(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
-        stopRumble();
+        controlify$stopRumble();
     }
 
     @ModifyExpressionValue(method = "continueDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z"))
     private boolean onAbortBreakingBlock(boolean original) {
         if (original)
-            stopRumble();
+            controlify$stopRumble();
 
         return original;
     }
 
     @Unique
-    private void startRumble(BlockState state) {
-        stopRumble();
+    private void controlify$startRumble(BlockState state) {
+        controlify$stopRumble();
 
         var effect = ContinuousRumbleEffect.builder()
                 .byTick(tick -> new RumbleState(
-                        0.02f + (float)Easings.easeInQuad(Math.min(1, state.getBlock().defaultDestroyTime() / 20f)) * 0.25f,
+                        0.02f + (float) Easings.easeInQuad(Math.min(1, state.getBlock().defaultDestroyTime() / 20f)) * 0.25f,
                         0.01f
                 ))
                 .minTime(1)
                 .build();
 
-        blockBreakRumble = effect;
-        ControlifyApi.get().getCurrentController()
-                .flatMap(ControllerEntity::rumble)
-                .ifPresent(rumble -> rumble.rumbleManager().play(RumbleSource.INTERACTION, effect));
+        controlify$blockBreakRumble = effect;
+        ControlifyApi.get().playRumbleEffect(RumbleSource.INTERACTION, effect);
     }
 
     @Unique
-    private void stopRumble() {
-        if (blockBreakRumble != null) {
-            blockBreakRumble.stop();
-            blockBreakRumble = null;
+    private void controlify$stopRumble() {
+        if (controlify$blockBreakRumble != null) {
+            controlify$blockBreakRumble.stop();
+            controlify$blockBreakRumble = null;
         }
     }
 }
--- src/main/java/dev/isxander/controlify/mixins/feature/virtualmouse/snapping/AbstractRecipeBookScreenMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/virtualmouse/snapping/AbstractRecipeBookScreenMixin.java
@@ -5,41 +5,28 @@ import dev.isxander.controlify.virtualmouse.SnapUtils;
 import net.minecraft.client.gui.screens.inventory.*;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.function.Consumer;
 
-//? if >=1.21.2 {
-/*@Mixin(AbstractRecipeBookScreen.class)
-*///?} else {
 @Mixin(value = {
         InventoryScreen.class,
         AbstractFurnaceScreen.class,
         CraftingScreen.class
 })
-//?}
 public abstract class AbstractRecipeBookScreenMixin<T extends AbstractContainerMenu>
         extends AbstractContainerScreenMixin<T> {
 
-    //? if >=1.21.2 {
-    /*@Shadow @Final private RecipeBookComponent<?> recipeBookComponent;
-    *///?} else {
-    @Shadow(remap = false, aliases = {"getRecipeBookComponent","m_5564_","method_2659"})
+    @Shadow(aliases = {"getRecipeBookComponent","m_5564_","method_2659"})
     public abstract RecipeBookComponent getRecipeBookComponent();
-    //?}
 
     @Override
     public void controlify$collectSnapPoints(Consumer<SnapPoint> consumer) {
         super.controlify$collectSnapPoints(consumer);
 
         SnapUtils.addRecipeSnapPoints(
-                //? if >=1.21.2 {
-                /*recipeBookComponent,
-                *///?} else {
                 getRecipeBookComponent(),
-                //?}
                 consumer
         );
     }
