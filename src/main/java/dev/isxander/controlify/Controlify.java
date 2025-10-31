EchoEllet/Controlify

commit 3d5c7383c4a4c07f35c598b1e3f5d890d343bb0a

Guides v2 (#647)

src/main/java/dev/isxander/controlify/Controlify.java
@@ -2,6 +2,13 @@ package dev.isxander.controlify;
 
 import com.mojang.blaze3d.Blaze3D;
 import dev.isxander.controlify.api.ControlifyApi;
+import dev.isxander.controlify.api.bind.ControlifyBindApi;
+import dev.isxander.controlify.api.entrypoint.InitContext;
+import dev.isxander.controlify.api.entrypoint.PreInitContext;
+import dev.isxander.controlify.api.guide.ContainerCtx;
+import dev.isxander.controlify.api.guide.GuideDomainRegistries;
+import dev.isxander.controlify.api.guide.GuideDomainRegistry;
+import dev.isxander.controlify.api.guide.InGameCtx;
 import dev.isxander.controlify.bindings.BindContext;
 import dev.isxander.controlify.bindings.ControlifyBindApiImpl;
 import dev.isxander.controlify.bindings.ControlifyBindings;
@@ -21,6 +28,7 @@ import dev.isxander.controlify.driver.sdl.SDLNativesLoader;
 import dev.isxander.controlify.driver.steamdeck.SteamDeckMode;
 import dev.isxander.controlify.driver.steamdeck.SteamDeckUtil;
 import dev.isxander.controlify.font.InputFontMapper;
+import dev.isxander.controlify.gui.guide.GuideDomains;
 import dev.isxander.controlify.gui.screen.*;
 import dev.isxander.controlify.debug.DebugProperties;
 import dev.isxander.controlify.ingame.ControllerPlayerMovement;
@@ -53,7 +61,6 @@ import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 import static dev.isxander.controlify.utils.ControllerUtils.wrapControllerError;
@@ -112,6 +119,8 @@ public class Controlify implements ControlifyApi {
         PlatformClientUtil.registerAssetReloadListener(inputFontMapper);
         PlatformClientUtil.registerAssetReloadListener(defaultBindManager);
         PlatformClientUtil.registerAssetReloadListener(controllerTypeManager);
+        PlatformClientUtil.registerAssetReloadListener(GuideDomains.IN_GAME);
+        PlatformClientUtil.registerAssetReloadListener(GuideDomains.CONTAINER);
 
         controllerHIDService = new ControllerHIDService();
         controllerHIDService.start();
@@ -152,6 +161,25 @@ public class Controlify implements ControlifyApi {
 
         PlatformClientUtil.addHudLayer(CUtil.rl("button_guide"), (graphics, deltaTracker) ->
                 inGameButtonGuide().ifPresent(guide -> guide.renderHud(graphics, deltaTracker.getGameTimeDeltaPartialTick(false))));
+
+        PlatformMainUtil.applyToControlifyEntrypoint(entrypoint -> {
+            try {
+                entrypoint.onControlifyPreInit(() -> new GuideDomainRegistries() {
+                    @Override
+                    public GuideDomainRegistry<InGameCtx> inGame() {
+                        return GuideDomains.IN_GAME;
+                    }
+
+                    @Override
+                    public GuideDomainRegistry<ContainerCtx> container() {
+                        return GuideDomains.CONTAINER;
+                    }
+                });
+            } catch (Throwable e) {
+                CUtil.LOGGER.error("Failed to run `onControlifyPreInit` on Controlify entrypoint: {}", entrypoint.getClass().getName(), e);
+            }
+        });
+        GuideDomains.freeze();
     }
 
     private void registerBuiltinPack(String id) {
@@ -221,7 +249,17 @@ public class Controlify implements ControlifyApi {
 
         PlatformMainUtil.applyToControlifyEntrypoint(entrypoint -> {
             try {
-                entrypoint.onControlifyInit(this);
+                entrypoint.onControlifyInit(new InitContext() {
+                    @Override
+                    public ControlifyBindApi bindings() {
+                        return ControlifyBindApiImpl.INSTANCE;
+                    }
+
+                    @Override
+                    public ControlifyApi controlify() {
+                        return Controlify.this;
+                    }
+                });
             } catch (Throwable e) {
                 CUtil.LOGGER.error("Failed to run `onControlifyInit` on Controlify entrypoint: {}", entrypoint.getClass().getName(), e);
             }
@@ -575,7 +613,7 @@ public class Controlify implements ControlifyApi {
             if (currentInputMode == InputMode.KEYBOARD_MOUSE) {
                 this.inGameButtonGuide = null;
             } else {
-                this.inGameButtonGuide = this.getCurrentController().map(c -> new InGameButtonGuide(c, Minecraft.getInstance().player)).orElse(null);
+                this.inGameButtonGuide = this.getCurrentController().map(c -> new InGameButtonGuide(c, minecraft)).orElse(null);
             }
         }
         if (Blaze3D.getTime() - lastInputSwitchTime < 20) {

src/main/java/dev/isxander/controlify/api/entrypoint/ControlifyEntrypoint.java
@@ -16,14 +16,13 @@ public interface ControlifyEntrypoint {
      * have not yet been discovered and constructed. This is the ideal
      * time to register events in preparation for controller discovery.
      */
-    default void onControlifyInit(ControlifyApi controlify) {
-        this.onControlifyPreInit(controlify);
-    }
+    void onControlifyInit(InitContext context);
 
 
-    @SuppressWarnings("DeprecatedIsStillUsed")
-    @Deprecated
-    default void onControlifyPreInit(ControlifyApi controlify) {
-    }
+    /**
+     * Called at the end of Controlify's client-side entrypoint.
+     * You can register guides here.
+     */
+    void onControlifyPreInit(PreInitContext context);
 
 }

src/main/java/dev/isxander/controlify/api/entrypoint/InitContext.java
@@ -1,0 +1,10 @@
+package dev.isxander.controlify.api.entrypoint;
+
+import dev.isxander.controlify.api.ControlifyApi;
+import dev.isxander.controlify.api.bind.ControlifyBindApi;
+
+public interface InitContext {
+    ControlifyBindApi bindings();
+
+    ControlifyApi controlify();
+}

src/main/java/dev/isxander/controlify/api/entrypoint/PreInitContext.java
@@ -1,0 +1,7 @@
+package dev.isxander.controlify.api.entrypoint;
+
+import dev.isxander.controlify.api.guide.GuideDomainRegistries;
+
+public interface PreInitContext {
+    GuideDomainRegistries guideRegistries();
+}

src/main/java/dev/isxander/controlify/api/bind/ControlifyBindApi.java
@@ -37,6 +37,17 @@ public interface ControlifyBindApi {
     InputBindingSupplier registerBinding(RegistryCallback callback, Predicate<ControllerEntity> filter);
 
     /**
+     * Creates a supplier for an input binding with the given bind ID.
+     * Useful when fetching bindings by ID, rather than by java reference, such is needed
+     * in cases of data-driven systems.
+     * {@link InputBindingSupplier#on(ControllerEntity)} may throw an exception if the binding does not exist,
+     * when it is attempted to be resolved.
+     * @param bindId the ID of the binding to create a supplier for
+     * @return the supplier for the binding
+     */
+    InputBindingSupplier createSupplier(ResourceLocation bindId);
+
+    /**
      * Get all the controller bindings for a vanilla key mapping.
      * This is populated using {@link InputBindingBuilder#addKeyCorrelation(KeyMapping)}
      * or {@link InputBindingBuilder#keyEmulation(KeyMapping)}.

src/main/java/dev/isxander/controlify/api/bind/InputBinding.java
@@ -52,7 +52,16 @@ public interface InputBinding {
      * The icon glyph of the input
      * @return the icon component
      */
-    Component inputIcon();
+    Component inputGlyph();
+
+    /**
+     * The icon glyph of the input
+     * @return the icon component
+     */
+    @Deprecated
+    default Component inputIcon() {
+        return this.inputGlyph();
+    }
 
     /**
      * Create a new state access with the given history required.

src/main/java/dev/isxander/controlify/api/bind/InputBindingSupplier.java
@@ -1,5 +1,7 @@
 package dev.isxander.controlify.api.bind;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.isxander.controlify.controller.ControllerEntity;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
@@ -37,4 +39,9 @@ public interface InputBindingSupplier {
      * @return id of the binding.
      */
     ResourceLocation bindId();
+
+    Codec<InputBindingSupplier> CODEC = ResourceLocation.CODEC.xmap(
+            bindId -> ControlifyBindApi.get().createSupplier(bindId),
+            InputBindingSupplier::bindId
+    );
 }

src/main/java/dev/isxander/controlify/api/buttonguide/ButtonRenderPosition.java
@@ -1,20 +1,0 @@
-package dev.isxander.controlify.api.buttonguide;
-
-/**
- * Where the guide should be rendered relative to the button.
- */
-@Deprecated
-public enum ButtonRenderPosition {
-    /**
-     * Renders outside the button the left edge of the screen.
-     */
-    LEFT,
-    /**
-     * Renders outside the button the right edge of the screen.
-     */
-    RIGHT,
-    /**
-     * Renders inside the button on the left of the button text.
-     */
-    TEXT
-}

commit 9c0919c316d13b2631702b90a3767ee7308a634c

chore: adapt the ControlifyEntrypoint breaking change introduced in https://github.com/isXander/Controlify/pull/647/files

src/main/java/dev/isxander/controlify/Controlify.java
@@ -2,6 +2,9 @@ package dev.isxander.controlify;
 
 import com.mojang.blaze3d.Blaze3D;
 import dev.isxander.controlify.api.ControlifyApi;
+import dev.isxander.controlify.api.bind.ControlifyBindApi;
+import dev.isxander.controlify.api.entrypoint.InitContext;
+import dev.isxander.controlify.api.entrypoint.PreInitContext;
 import dev.isxander.controlify.bindings.BindContext;
 import dev.isxander.controlify.bindings.ControlifyBindApiImpl;
 import dev.isxander.controlify.bindings.ControlifyBindings;
@@ -159,6 +162,16 @@ public class Controlify implements ControlifyApi {
 
         PlatformClientUtil.addHudLayer(CUtil.rl("button_guide"), (graphics, tickDelta) ->
                 inGameButtonGuide().ifPresent(guide -> guide.renderHud(graphics, tickDelta)));
+
+        PlatformMainUtil.applyToControlifyEntrypoint(entrypoint -> {
+            try {
+                entrypoint.onControlifyPreInit(new PreInitContext() {
+                });
+            } catch (Throwable e) {
+                CUtil.LOGGER.error("Failed to run `onControlifyPreInit` on Controlify entrypoint: {}", entrypoint.getClass().getName(), e);
+            }
+        });
+        //GuideDomains.freeze();
     }
 
     private void registerBuiltinPack(String id) {
@@ -196,14 +209,6 @@ public class Controlify implements ControlifyApi {
                     ScreenProcessorProvider.provide(screen).render(controller, graphics, tickDelta);
                 }));
 
-        PlatformMainUtil.applyToControlifyEntrypoint(entrypoint -> {
-            try {
-                entrypoint.onControlifyInit(this);
-            } catch (Throwable e) {
-                CUtil.LOGGER.error("Failed to run `onControlifyInit` on Controlify entrypoint: {}", entrypoint.getClass().getName(), e);
-            }
-        });
-
         if (config().globalSettings().isQuietMode()) {
             // Use GLFW to probe for controllers without asking for natives
             boolean controllersConnected = GLFWControllerManager.areControllersConnected();
@@ -233,6 +238,24 @@ public class Controlify implements ControlifyApi {
         if (this.config().globalSettings().useEnhancedSteamDeckDriver) {
             doSteamDeckChecks();
         }
+
+        PlatformMainUtil.applyToControlifyEntrypoint(entrypoint -> {
+            try {
+                entrypoint.onControlifyInit(new InitContext() {
+                    @Override
+                    public ControlifyBindApi bindings() {
+                        return ControlifyBindApiImpl.INSTANCE;
+                    }
+
+                    @Override
+                    public ControlifyApi controlify() {
+                        return Controlify.this;
+                    }
+                });
+            } catch (Throwable e) {
+                CUtil.LOGGER.error("Failed to run `onControlifyInit` on Controlify entrypoint: {}", entrypoint.getClass().getName(), e);
+            }
+        });
     }
 
     private void doSteamDeckChecks() {

src/main/java/dev/isxander/controlify/api/entrypoint/ControlifyEntrypoint.java
@@ -16,14 +16,13 @@ public interface ControlifyEntrypoint {
      * have not yet been discovered and constructed. This is the ideal
      * time to register events in preparation for controller discovery.
      */
-    default void onControlifyInit(ControlifyApi controlify) {
-        this.onControlifyPreInit(controlify);
-    }
+    void onControlifyInit(InitContext context);
 
 
-    @SuppressWarnings("DeprecatedIsStillUsed")
-    @Deprecated
-    default void onControlifyPreInit(ControlifyApi controlify) {
-    }
+    /**
+     * Called at the end of Controlify's client-side entrypoint.
+     * You can register guides and input bindings here.
+     */
+    void onControlifyPreInit(PreInitContext context);
 
 }

src/main/java/dev/isxander/controlify/api/entrypoint/InitContext.java
@@ -1,0 +1,10 @@
+package dev.isxander.controlify.api.entrypoint;
+
+import dev.isxander.controlify.api.ControlifyApi;
+import dev.isxander.controlify.api.bind.ControlifyBindApi;
+
+public interface InitContext {
+    ControlifyBindApi bindings();
+
+    ControlifyApi controlify();
+}

src/main/java/dev/isxander/controlify/api/entrypoint/PreInitContext.java
@@ -1,0 +1,5 @@
+package dev.isxander.controlify.api.entrypoint;
+
+public interface PreInitContext {
+
+}

src/main/java/dev/isxander/controlify/mixins/feature/screenop/vanilla/MerchantScreenMixin.java
@@ -24,6 +24,8 @@ public class MerchantScreenMixin {
      */
     @ModifyExpressionValue(method = "mouseScrolled", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/MerchantScreen;canScroll(I)Z"))
     // target is just before we calculate new scroll offset
+    // TODO: (PORT): Backport this commit if needed:
+    // https://github.com/isXander/Controlify/commit/cb875cbd22c2b7d23369f110419dcb0bb46e020b
     private boolean accumulateScrolling(boolean canScroll, @Local(ordinal = 2, argsOnly = true) LocalDoubleRef scrollYRef) {
         if (canScroll) {
             // we want to target the beginning of the inner if statement,